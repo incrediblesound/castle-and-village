18961d598ead8c301fd70a9f96a2df9c01a10107
James H. Edwards <james.howe.edwards@gmail.com>
2014-10-18 13:48:51 -0700

    refactor disaster controller

--- public/src/disasters.js
+++ public/src/disasters.js
@@ -1,35 +1,20 @@
 var DisasterController = function(){
   System.call(this);
 
-  this.disasterMap = {
-    badits: [1, 0],
-    wilderness: [1, 1]
-  }
-
   this.disasters = {
-    0: [],
-    1: [
-      function(){
-        if(window.gameState.gameController.units['barracks'].knights < 7){
-          // showAlert('Bandits!','Bandits attacked and stole food!');
-          alert('Bandits attacked and stole food!');
-          window.gameState.gameController.units['village'].food -= 10;
-        } else {
-          alert('Bandits attacked, but you fought them off!')
-        }
-      },
-      function(){
-        if(window.gameState.gameController.units['domain'].fields > 1 && window.gameState.gameController.units['village'].energy < 5){
-          // showAlert('Field Lost','One of your fields was taken back by the wilderness!');
-          alert('One of your fields was taken back by the wilderness!');
-          window.gameState.gameController.units['domain'].fields -= 1;
-          window.gameState.gameController.units['domain'].forests += 1;
-        }
-      },
-    ],
-    2: [
-      function(){}
-    ]
+    Wolf: function(){
+      window.gameState.gameController.changeStat('domain','Peasants', -1);
+      window.gameState.gameController.message('One of your peasants was eaten by a wolf!', 'red');     
+    },
+    Bandits: function(){
+      window.gameState.gameController.message('Bandits attacked and stole food!');
+      window.gameState.gameController.units['village'].food -= 10;
+    },
+    Wilderness: function(){
+      window.gameState.gameController.message('One of your fields was taken back by the wilderness!');
+      window.gameState.gameController.units['domain'].fields -= 1;
+      window.gameState.gameController.units['domain'].forests += 1;
+    }
   }
 }
 
@@ -37,17 +22,25 @@ DisasterController.prototype = Object.create(System.prototype);
 DisasterController.prototype.contructor = DisasterController;
 
 DisasterController.prototype.doDisaster = function(name){
-  var location = this.disasterMap[name];
-  this.disasters[location[0]][location[1]]();
+  this.disasters[name]();
 }
 
 DisasterController.prototype.step = function(){
-  var num = Math.floor(Math.random()*10);
-    if(num < 2){
-      var disasters = this.disasters[window.gameState.gameController.stats.level];
-      if(disasters.length > 0){
-        var select = Math.floor(Math.random()*disasters.length);
-        disasters[select]();
+  var check = randomize(10);
+  if(check > 4){
+    if(window.gameState.gameController.stats.level === 0){
+      if(!window.gameState.gameController.milestoneIsComplete('GatherWood') && 
+          window.gameState.gameController.milestoneIsComplete('SixPeasants')){
+        this.doDisaster('Wolf');
+      }
+      else if(!window.gameState.gameController.milestoneIsComplete('WalledIn') && 
+               window.gameState.gameController.milestoneIsComplete('TwoHuts')){
+        this.doDisaster('Bandits');
       }
     }
+  }
+}
+
+function randomize(max){
+  return !!max ? Math.floor(Math.random()*max) : Math.round(Math.random())
 }
